564eeff023337e784807a022f5d0fa59e193bee9
Fredrik Fornwall <fredrik@fornwall.net>
2020-02-24 14:54:24 +0100

    Add file upload from browser

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from io import TextIOWrapper
 from flask import Flask, request
 from flask_talisman import Talisman
 from json.decoder import JSONDecodeError
@@ -24,14 +25,33 @@ def get():
 @app.route("/schema-builder", methods=["POST"], strict_slashes=False)
 def generate_schema():
     """Return a generated schema built from posted HTTP recordings."""
+    if request.headers["content-type"].startswith("multipart/form-data"):
+        uploaded_file = request.files["file"]
+        uploaded_file.seek(0)
+        file_content = TextIOWrapper(uploaded_file)
+    else:
+        file_content = request.stream
+
     try:
-        http_exchanges = HttpExchangeReader.from_jsonl(request.stream)
+        http_exchanges = HttpExchangeReader.from_jsonl(file_content)
         openapi_schema = meeshkan.build_schema_batch(http_exchanges)
         response_text = yaml.safe_dump(openapi_schema)
-        return response_text, 200, {"content-type": "application/vnd.oai.openapi"}
+        return (
+            response_text,
+            200,
+            {
+                "content-type": "application/vnd.oai.openapi",
+                "content-disposition": 'attachment; filename="openapi-schema.yml"',
+            },
+        )
     except JSONDecodeError:
         response_text = "Content is not JSON\r\n"
         return response_text, 400, {"content-type": "text/plain"}
+
+
+@app.route("/_ah/warmup")
+def warmup():
+    return "OK"
 
 
 if __name__ == "__main__":
--- main_test.py
+++ main_test.py
@@ -5,15 +5,20 @@ def test_index():
     main.app.testing = True
     client = main.app.test_client()
 
-    r = client.get("/")
+    r = client.get("/schema-builder", base_url="https://localhost")
     assert r.status_code == 200
-    assert r.data.decode("utf-8").startswith("Meeshkan schema builder")
+    assert r.data.decode("utf-8") == open("index.html").read()
 
 
 def test_post_non_json():
     main.app.testing = True
     client = main.app.test_client()
 
-    r = client.post("/", data="not json")
+    r = client.post(
+        "/schema-builder",
+        data="not json",
+        base_url="https://localhost",
+        headers={"content-type": "application/x-www-form-urlencoded"},
+    )
     assert r.status_code == 400
     assert r.data.decode("utf-8") == "Content is not JSON\r\n"
